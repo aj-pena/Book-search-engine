4f2dc8bd18db6d30c60ae2078f07695b2c61d233
Andres Pena <penchjpc@gmail.com>
2021-11-19 22:35:25 -0500

    created resolvers for query and for mutations login and addUser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,27 +1,50 @@
-const { Class } = require('../models');
-const {User, Book} = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const {User} = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    me: async (parent, {userId}) => {
-      return await User.findOne({_id:userId});
+    me: async (parent, args, context) => {
+      if (context.user){
+      return await User.findOne({_id: context.user._id});
+      }
+      throw new AuthenticationError('You need to be logged in!')
     }    
   },
 
   Mutation: {
-    login: async () => {
+    login: async (parent, {email, password}) => {
+      const user = await User.findOne({ email });
+      if(!user){
+        throw new AuthenticationError('No user with this email found!')
+      }
+      
+      const correctPw = await user.isCorrectPassword(password);
 
-    }
-    addUser: async () => {
+      if(!correctPw){
+        throw new AuthenticationError('Incorrect password!');
+      }
 
-    }
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, {name, email, password}) => {
+      const user = await User.create( { name, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
     saveBook: async (parent, args, context) => {
         if(context.user){
-
+          
         }
-    }
-    removeBook: async () => {
-
+    },
+    removeBook: async (parent, args) => {
+      return await User.findOneAndUpdate(
+        {_id: user._id},
+        {$pull: {savedBooks: {bookId:args.bookId} } },
+        { new: true }
+        )
     }
 
   },
