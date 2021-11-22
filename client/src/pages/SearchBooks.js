e06945a399e4c0f86875213095e6cca90bfd0bc1
Andres Pena <penchjpc@gmail.com>
2021-11-22 13:20:39 -0500

    delete book button successful

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -69,12 +69,12 @@ const SearchBooks = () => {
 
     try {
       const response = await addBook({
-        variables: [...savedBookIds, bookId]  
+        variables: { bookData: {...bookToSave} }  
       });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
